a2a830d6ae392034ea38caea14c48f2444b5ea04
preyanshu <118627824+preyanshu@users.noreply.github.com>
2023-08-03 02:45:27 +0530

    Add files via upload
    added single product page with images

--- src/components/spimage.js
+++ src/components/spimage.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const SpImage  = () => {
+const SpImage  = ({imgs = [{ url:"" }]}) => {
+  
+  const [mainImg, setMainImg] = useState(imgs[0])
+
   return (
     <div className='spimg'>
-      <h1>img</h1>
+      <div>
+        {
+          imgs.map((curElm, index) => {
+            return(
+              <figure>
+                <img src={curElm.url} alt={curElm.filename} className='spimg1' key={index} onClick={() =>{ setMainImg(curElm)}} />
+              </figure>
+            )
+          })
+        }
+      </div>
+      <div>
+        <img src={mainImg.url} className='spimg2' alt={imgs[0].filename} />
+      </div>
     </div>
   )
 }
